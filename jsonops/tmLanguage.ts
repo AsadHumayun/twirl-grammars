862ffc84c14e8ebc656f9b8a28fe5c32215b25b4
Asad Humayun <105254706+AsadHumayun@users.noreply.github.com>
2026-03-24 08:33:05 +0000

    feat: construct using ts

--- jsonops/tmLanguage.ts
+++ jsonops/tmLanguage.ts
@@ -1,3 +1,18 @@
-interface TextMateLanguage {
-	
+interface Pattern {
+	name: string;
+	includes: string[];
 }
+
+interface TextMatePattern {
+	name: string;
+	patterns: Pattern[];
+}
+
+interface TextMateLanguage {
+	["$schema"]: string;
+	name: string;
+	scopeName: string;
+	fileTypes: string[];
+}
+
+
